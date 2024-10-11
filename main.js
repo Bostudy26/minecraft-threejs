e97dd8e490d34378dc4ad55ea6788719012f242e
Bo <boprojectbo@gmail.com>
2024-10-11 16:32:05 +0200

    Feat: Setups light + world + animate

--- main.js
+++ main.js
@@ -1,24 +1,65 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/Addons.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+// Renderer setup
+const renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth , window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-setupCounter(document.querySelector('#counter'))
+// camera setup
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight);
+camera.position.set ( 2,2,2);
+camera.lookAt (0,0,0);
+
+
+const controls = new OrbitControls(camera, renderer.domElement);
+// scene setup
+const scene =  new THREE.Scene();
+const geometry = new THREE.BoxGeometry();
+
+// MeshBasicMaterial cannot to be light.
+const material = new THREE.MeshLambertMaterial({color: 0x00d000});
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);  
+
+function setupLights() {
+  const light1 = new THREE.DirectionalLight();
+  light1.position.set(1,1,1);
+  scene.add(light1);
+
+  const light2 = new THREE.DirectionalLight();
+  light2.position.set(-1,1,-0,5);
+  scene.add(light2);
+
+  const ambient = new THREE.AmbientLight();
+  ambient.intensity = 0.1;
+  scene.add(ambient);
+
+}
+
+function setupWorld(size) {
+  for (let x = 0; x < size; x++) {
+    for (let z = 0; z < size; z++) {
+      const cube = new THREE.Mesh(geometry, material);
+      cube.position.set(x, 0 , z);
+      scene.add(cube);
+    }
+  }
+}
+
+// render loop
+function animate() {
+  requestAnimationFrame(animate);
+  renderer.render(scene, camera);
+}
+
+window.addEventListener('resize', () => { 
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+})
+
+setupLights();
+setupWorld(32);
+animate();
