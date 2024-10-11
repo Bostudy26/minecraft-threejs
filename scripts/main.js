950aa814720c382e8a55cdfe65eca37079f6f9c3
Bo <boprojectbo@gmail.com>
2024-10-11 19:45:44 +0200

    Feat: class of World

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,30 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/Addons.js';
+import { World } from './world';
 
 // Renderer setup
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth , window.innerHeight);
+renderer.setClearColor(0x80a0e0);
 document.body.appendChild(renderer.domElement);
 
 // camera setup
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight);
-camera.position.set ( 2,2,2);
-camera.lookAt (0,0,0);
+camera.position.set ( -32,2,-32);
+// camera.lookAt (0,0,0);
 
 
 const controls = new OrbitControls(camera, renderer.domElement);
+controls.target.set(16,0,16);
+controls.update();
+
 // scene setup
 const scene =  new THREE.Scene();
-const geometry = new THREE.BoxGeometry();
+const world = new World(32);
+world.generate();
+scene.add(world);
 
-// MeshBasicMaterial cannot to be light.
-const material = new THREE.MeshLambertMaterial({color: 0x00d000});
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);  
 
 function setupLights() {
   const light1 = new THREE.DirectionalLight();
@@ -39,13 +42,7 @@ function setupLights() {
 }
 
 function setupWorld(size) {
-  for (let x = 0; x < size; x++) {
-    for (let z = 0; z < size; z++) {
-      const cube = new THREE.Mesh(geometry, material);
-      cube.position.set(x, 0 , z);
-      scene.add(cube);
-    }
-  }
+
 }
 
 // render loop
@@ -61,5 +58,4 @@ window.addEventListener('resize', () => {
 })
 
 setupLights();
-setupWorld(32);
 animate();
